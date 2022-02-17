441af79a769009bb5fb2c61eb0069306b6a0dced
[상품개발팀]서민규 <minkyu.seo@lotte.net>
2022-02-18 08:13:55 +0900

    admin 반영

---
+++ src/main/java/com/fitcloude/expertadmin/ExpertAdminApplication.java
@@ -0,0 +1,13 @@
+package com.fitcloude.expertadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitcloude/expertadmin/ExpertAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitcloude.expertadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
